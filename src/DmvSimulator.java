0d99cee48c5ade3be0ea49ec40ccd9694fd02223
AnzilottiChristopher <anzilottichr@gmail.com>
2023-03-02 16:50:23 -0500

    Got Random Number

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,7 +1,19 @@
+import java.security.SecureRandom;
+
 public class DmvSimulator 
 {
     public static void main(String[] args) throws Exception 
     {
+        //Welcom Message
+        System.out.println("Welcome to the DMV! We will assign you a random number");
+        System.out.println("Please wait until your number is called");
         
+        //User's number
+        SecureRandom randomNumber = new SecureRandom();
+        int usersNumber = 1 + randomNumber.nextInt(100);
+        for(int count = usersNumber; count < 100; count++)
+        {
+            System.out.println("Number: " + (count + 1) + " is next");
+        }
     }
 }
